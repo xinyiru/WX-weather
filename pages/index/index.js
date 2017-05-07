46072c01b2b63c02d32595dc8c384a6ba7b61a0b
2506738200@qq.com <xin360360>
2017-05-07 15:26:18 +0800

    'weather'

---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+//index.js
+//获取应用实例
+
+var util=require('../../utils/util.js');
+
+Page({
+  data: {
+    weather: {}
+  },
+  //事件处理函数
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+
+    //调用应用实例的方法获取全局数据
+    util.loadWeatherData(function(data){
+      //更新数据
+      that.setData({
+        weather:data
+      });
+      wx.setNavigationBarTitle({
+        title: data.city + ' 天气预报',
+        
+      })
+    })
+  }
+})
